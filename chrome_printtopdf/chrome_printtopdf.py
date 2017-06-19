97c25b5607829fde82fe767db4ab53f3b5e1158a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2017-06-19 20:21:31 +0200

    Add synchronous ChromeContextManager for use in Threads

--- chrome_printtopdf/chrome_printtopdf.py
+++ chrome_printtopdf/chrome_printtopdf.py
@@ -4,6 +4,8 @@ import json
 import logging
 from io import BytesIO
 import tempfile
+import time
+import subprocess
 
 import aiohttp
 
@@ -11,6 +13,16 @@ logger = logging.getLogger(__name__)
 
 DEFAULT_HOST = '127.0.0.1'
 DEFAULT_PORT = 9222
+
+
+def get_loop():
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError:
+        pass
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    return loop
 
 
 async def get_debug_url(session, host=DEFAULT_HOST, port=DEFAULT_PORT):
@@ -25,18 +37,19 @@ async def get_debug_url(session, host=DEFAULT_HOST, port=DEFAULT_PORT):
 def send_message(ws, command):
     logger.debug('-> %s', command[1])
     ws.send_str(json.dumps(command[1]))
-    return command[0], command[1]['id']
+    return command[0], command[1]['id'], command[2]
 
 
 async def send_print_command(ws, print_url):
     command_list = [
-        (None, {"id": 1, "method": "Page.enable", "params": {}}),
+        (None, {"id": 1, "method": "Page.enable", "params": {}}, 0),
         ('Page.frameStoppedLoading', {"id": 2, "method": "Page.navigate",
             "params": {
                 "url": print_url
             }
-        }),
-        (None, {"id": 3, "method": "Page.printToPDF", "params": {}})
+        }, 1),
+        (None, {'id': 3, "method": "Page.stopLoading"}, 1),
+        (None, {"id": 4, "method": "Page.printToPDF", "params": {}}, 0),
     ]
     pdf_bytes = None
     wait_for_event = send_message(ws, command_list[0])
@@ -50,6 +63,9 @@ async def send_print_command(ws, print_url):
             if ((wait_for_event[0] is None and
                         result.get('id') == wait_for_event[1]) or
                         result.get('method') == wait_for_event[0]):
+
+                await asyncio.sleep(wait_for_event[2])
+
                 if index < len(command_list):
                     wait_for_event = send_message(ws, command_list[index])
                     index += 1
@@ -84,7 +100,7 @@ async def wait_for_port(ip, port, num_tries=3, timeout=5, loop=None):
 
 async def get_pdf(url, loop=None, host=DEFAULT_HOST, port=DEFAULT_PORT):
     if loop is None:
-        loop = asyncio.get_event_loop()
+        loop = get_loop()
     async with aiohttp.ClientSession(loop=loop) as session:
         debugger_url = await get_debug_url(session, host=host, port=port)
         logger.debug('Connecting to %s', debugger_url)
@@ -95,23 +111,33 @@ async def get_pdf(url, loop=None, host=DEFAULT_HOST, port=DEFAULT_PORT):
             return BytesIO(pdf_bytes)
 
 
-class ChromeContextManager:
-    def __init__(self, loop=None, chrome_binary='/usr/local/bin/chromium',
+class BaseChromeContextManager:
+    def __init__(self, chrome_binary='/usr/local/bin/chromium',
                              host=DEFAULT_HOST, port=9222):
-        if loop is None:
-            loop = asyncio.get_event_loop()
-        self.loop = loop
         self.chrome_binary = chrome_binary
         self.host = host
         self.port = port
 
-    async def __aenter__(self):
+    def prepare_call(self):
         self.temp_dir = tempfile.TemporaryDirectory()
-        self.args = [self.chrome_binary, '--headless', '--disable-gpu',
+        self.args = [self.chrome_binary,
+            '--headless',
+            '--disable-gpu',
             '--remote-debugging-port=%s' % self.port,
             '--remote-debugging-address=%s' % self.host,
             '--user-data-dir=%s' % self.temp_dir.name]
         logger.debug('Starting chrome: %s', self.chrome_binary)
+
+
+class AsyncChromeContextManager(BaseChromeContextManager):
+    def __init__(self, loop=None, **kwargs):
+        if loop is None:
+            loop = get_loop()
+        self.loop = loop
+        super().__init__(**kwargs)
+
+    async def __aenter__(self):
+        asyncio.get_child_watcher().attach_loop(self.loop)
         self.proc = await asyncio.create_subprocess_exec(*self.args,
                                                          loop=self.loop)
         logger.debug('Started, waiting for debug port...')
@@ -127,10 +153,30 @@ class ChromeContextManager:
         logger.debug('Chrome terminated.')
 
 
+class ChromeContextManager(BaseChromeContextManager):
+    def __enter__(self):
+        self.prepare_call()
+
+        self.proc = subprocess.Popen(
+                self.args,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+        )
+        time.sleep(1)
+        logger.debug('Chrome started.')
+
+    def __exit__(self, exc_type, exc, tb):
+        logger.debug('Terminating chrome...')
+        self.proc.terminate()
+        self.proc.wait()
+        self.temp_dir.cleanup()
+        logger.debug('Chrome terminated.')
+
+
 async def get_pdf_with_chrome(url, loop=None, **chrome_options):
     if loop is None:
-        loop = asyncio.get_event_loop()
-    async with ChromeContextManager(loop, **chrome_options):
+        loop = get_loop()
+    with ChromeContextManager(**chrome_options):
         return await get_pdf(url, loop,
                              host=chrome_options.get('host', DEFAULT_HOST),
                              port=chrome_options.get('port', DEFAULT_PORT),
@@ -138,11 +184,11 @@ async def get_pdf_with_chrome(url, loop=None, **chrome_options):
 
 
 def get_pdf_sync(url, **options):
-    loop = asyncio.get_event_loop()
+    loop = get_loop()
     return loop.run_until_complete(get_pdf(url, loop=loop, **options))
 
 
 def get_pdf_with_chrome_sync(url, **chrome_options):
-    loop = asyncio.get_event_loop()
+    loop = get_loop()
     return loop.run_until_complete(get_pdf_with_chrome(url, loop=loop,
                                                        **chrome_options))
